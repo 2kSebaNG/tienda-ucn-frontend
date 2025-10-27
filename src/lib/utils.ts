be201aa7b21911718739b90cd1a34e0b437e8968
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2025-10-27 20:21:04 -0300

    feat(utils): add formatDate function for date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -43,3 +43,12 @@ export function thousandSeparatorPipe(num: number): string {
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export function formatDate(date: string): string {
+  const parsedDate = new Date(date);
+  const day = String(parsedDate.getDate()).padStart(2, "0");
+  const month = String(parsedDate.getMonth() + 1).padStart(2, "0");
+  const year = parsedDate.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
